756d576030571f931a0046404be27fbad214f046
Yasin Durusoy <63608199+ysndrsy@users.noreply.github.com>
2021-10-22 14:42:08 +0300

    Update yap.py

--- yap.py
+++ yap.py
@@ -1,5 +1,11 @@
 from tkinter import *
 
+import os
+
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
 window = Tk()
 
 window.title("Welcome to LikeGeeks app")
